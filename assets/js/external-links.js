446503cb5c27964f693b0d931b52ff18f862b159
Lewis Goddard <lewisgoddard@eustasy.org>
2015-07-10 22:04:25 +0100

    Fix "Expected '===' and instead saw '=='."

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -2,7 +2,7 @@
 // Open external links in a new tab.
 $(function () {
 	$('a').each(function () {
-		if ( $(this).href.indexOf(window.location.host) == -1 ) {
+		if ( $(this).href.indexOf(window.location.host) === -1 ) {
 			$(this).attr('target', '_blank');
 		}
 	});
