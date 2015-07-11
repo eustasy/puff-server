69e3426b980dc6bc4358e9030c26b739d8571d59
Lewis Goddard <lewisgoddard@eustasy.org>
2015-07-11 13:44:13 +0100

    Fix: Expected 1 newline after opening brace; 2 found

--- _puff/hooks/preload/strip-php.php
+++ _puff/hooks/preload/strip-php.php
@@ -4,7 +4,6 @@ if (
 	$Sitewide['Settings']['Strip PHP from URLs'] &&
 	substr($Sitewide['Request']['Path'], -4, 4) == '.php'
 ) {
-
 	$Sitewide['Request']['Trimmed'] = $Sitewide['Request']['Scheme'].'://'.$Sitewide['Request']['Host'].substr($Sitewide['Request']['Path'], 0, -4);
 	if ( $Sitewide['Request']['Query'] ) {
 		$Sitewide['Request']['Trimmed'] .= '?'.$Sitewide['Request']['Query'];
@@ -12,8 +11,6 @@ if (
 	if ( $Sitewide['Request']['Fragment'] ) {
 		$Sitewide['Request']['Trimmed'] .= '#'.$Sitewide['Request']['Fragment'];
 	}
-
 	header ('HTTP/1.1 301 Moved Permanently');
 	header ('Location: '.$Sitewide['Request']['Trimmed']);
-
 }
