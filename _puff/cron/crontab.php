ca94e2c044e0786480359f293ba967531857b4e4
Lewis Goddard <lewisgoddard@eustasy.org>
2015-07-27 20:59:40 +0100

    Add Crontab Generation

---
+++ _puff/cron/crontab.php
@@ -0,0 +1,5 @@
+<?php
+	echo ' *  * * * * php '.__DIR__.'/minute.php'."\n";
+	echo '03  * * * * php '.__DIR__.'/hourly.php'."\n";
+	echo '03 02 * * * php '.__DIR__.'/daily.php' ."\n";
+	echo '03 02 * * 1 php '.__DIR__.'/weekly.php'."\n";
