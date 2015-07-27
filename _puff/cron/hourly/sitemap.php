9f0975baf56e6da44f28706d7e0e65d4adeb2b87
Lewis Goddard <lewisgoddard@eustasy.org>
2015-07-27 16:34:58 +0100

    Update sitemap.php

--- _puff/cron/hourly/sitemap.php
+++ _puff/cron/hourly/sitemap.php
@@ -1,5 +1,5 @@
 <?php
-	require_once __DIR__.'/_puff/sitewide.php';
+	require_once __DIR__.'/../../sitewide.php';
 
 	foreach (glob_recursive($Sitewide['Root'].'*.php', 0, true) as $File) {
 		echo $File.'<br>';
