ef8e84aa69d0c3e306bcac7d3149c564ec4f9524
Lewis Goddard <lewisgoddard@eustasy.org>
2015-07-28 09:34:58 +0100

    Separate crons more

--- _puff/cron/crontab.php
+++ _puff/cron/crontab.php
@@ -1,5 +1,5 @@
 <?php
 	echo ' *  * * * * php '.__DIR__.'/minute.php'."\n";
-	echo '03  * * * * php '.__DIR__.'/hourly.php'."\n";
-	echo '03 02 * * * php '.__DIR__.'/daily.php' ."\n";
-	echo '03 02 * * 1 php '.__DIR__.'/weekly.php'."\n";
+	echo '01  * * * * php '.__DIR__.'/hourly.php'."\n";
+	echo '11 02 * * * php '.__DIR__.'/daily.php' ."\n";
+	echo '22 01 * * 0 php '.__DIR__.'/weekly.php'."\n";
