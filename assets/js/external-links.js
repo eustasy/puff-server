259a7d79e4f6118feb473d7ddeeed9a86bc5fca1
Lewis Goddard <lewisgoddard@eustasy.org>
2015-07-28 09:44:05 +0100

    Fix equivalent operator

--- assets/js/external-links.js
+++ assets/js/external-links.js
@@ -8,7 +8,7 @@ $(function () {
 			// Check the host isn't in the link.
 			this.href.indexOf(window.location.host) === -1 &&
 			// Check it's not a magnet or anything stupid.
-			this.href.substring(0, 4).toLowerCase() == 'http'
+			this.href.substring(0, 4).toLowerCase() === 'http'
 		) {
 			$(this).attr('target', '_blank');
 		}
