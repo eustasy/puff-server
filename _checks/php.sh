c87077951a060a9f298979d9a7e1e12ffe7b740d
Lewis Goddard <lewisgoddard@eustasy.org>
2015-09-20 00:35:40 +0100

    More output

--- _checks/php.sh
+++ _checks/php.sh
@@ -1,6 +1,7 @@
+find ./ -name '*.php' -exec php -l {} \;
+
 if find ./ -name '*.php' -exec php -l {} \; |grep -v 'No syntax error'
 then
 	echo 'There are PHP errors.'
-	# find ./ -name '*.php' -exec php -l {} \;
 	exit 1;
 fi
