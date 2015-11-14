6a1ad64aa445379c3403ea6b6c6d120c07cd18d2
Lewis Goddard <lewisgoddard@eustasy.org>
2015-11-14 11:16:17 +0000

    Update headers.php

--- _puff/hooks/preload/headers.php
+++ _puff/hooks/preload/headers.php
@@ -4,9 +4,7 @@
 // X-Content-Security-Policy
 // X-WebKit-CSP
 if ( $Sitewide['Settings']['Content Security Policy Header'] ) {
-	$CSP = 'default-src \'self\'; script-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net 1.2.3.8 *.google-analytics.com; style-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net; font-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net; img-src *; media-src * mediastream:; frame-src *; object-src *; child-src *; frame-ancestors \'none\'; form-action \'self\'; connect-src \'self\' cdn.jsdelivr.net 1.2.3.8 *.google-analytics.com;';
-	// TODO Add CSP breach reporting.
-	// report-uri '\'//'.$Sitewide['Root'].'/api?csp-report\';';
+	$CSP = 'default-src \'self\'; script-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net 1.2.3.8 *.google-analytics.com; style-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net; font-src \'self\' \'unsafe-inline\' cdn.jsdelivr.net; img-src *; media-src * mediastream:; frame-src *; object-src *; child-src *; frame-ancestors \'none\'; form-action \'self\'; connect-src \'self\' cdn.jsdelivr.net 1.2.3.8 *.google-analytics.com; report-uri \'//'.$Sitewide['Root'].'/api/csp-report.php\';';
 	header('X-WebKit-CSP: '.$CSP);
 	header('X-Content-Security-Policy: '.$CSP);
 	header('Content-Security-Policy: '.$CSP);
