55cda00853a0ee3592fb1b565ec4cb34b881a3d3
Lewis Goddard <lewisgoddard@eustasy.org>
2016-02-14 00:45:02 +0000

    Update ends_with.php

--- _functions/core/ends_with.php
+++ _functions/core/ends_with.php
@@ -3,6 +3,8 @@
 function ends_with($string, $test) {
 	$strlen = strlen($string);
 	$testlen = strlen($test);
-	if ($testlen > $strlen) return false;
+	if ($testlen > $strlen) {
+		return false;
+	}
 	return substr_compare($string, $test, $strlen - $testlen, $testlen) === 0;
 }
