6e075f6a23959854e8040fd97c91fcf947afc3f0
Lewis Goddard <lewisgoddard@eustasy.org>
2016-09-15 17:35:43 +0100

    Tidy settings and functions

--- _cron/hourly/sitemap.php
+++ _cron/hourly/sitemap.php
@@ -1,8 +1,5 @@
 <?php
 
-// TODO Variable should be in Config.
-$SitemapPagination = 10;
-
 // For each PHP File
 foreach (glob_recursive($Sitewide['Root'].'*.php', 0, true) as $File) {
 	// Get page variables.
@@ -17,48 +14,11 @@ foreach (glob_recursive($Sitewide['Root'].'*.php', 0, true) as $File) {
 	}
 }
 
-// TODO Move to function file.
-function Puff_Sitemap_Make($SitemapPages) {
-	$Sitemap = '<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL;
-	$Sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.PHP_EOL;
-	foreach ( $SitemapPages as $Item ) {
-		// TODO Responsive Priority and ChangeFreq
-		$Sitemap .= '	<url>'.PHP_EOL;
-		$Sitemap .= '		<loc>'.$Item['Link'].'</loc>'.PHP_EOL;
-		if (
-			!empty($Item['Published']) &&
-			$Item['Published'] != '1970-01-01'
-		) {
-			$Sitemap .= '		<lastmod>'.$Item['Published'].'</lastmod>'.PHP_EOL;
-		}
-		$Sitemap .= '		<priority>1</priority>'.PHP_EOL;
-		$Sitemap .= '		<changefreq>daily</changefreq>'.PHP_EOL;
-		$Sitemap .= '	</url>'.PHP_EOL;
-	}
-	$Sitemap .= '</urlset>'.PHP_EOL;
-	return $Sitemap;
-}
-
-// TODO Move to function file.
-function Puff_Sitemap_Index($SitemapCount) {
-	global $Sitewide;
-	$Sitemap = '<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL;
-	$Sitemap .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.PHP_EOL;
-	for ($i = 1; $i < $SitemapCount; $i++) {
-		$Sitemap .= '	<sitemap>'.PHP_EOL;
-		$Sitemap .= '		<loc>'.$Sitewide['Settings']['Site Root'].'sitemap_'.$i.'.xml'.'</loc>'.PHP_EOL;
-		$Sitemap .= '		<lastmod>'.date('c').'</lastmod>'.PHP_EOL;
-		$Sitemap .= '	</sitemap>'.PHP_EOL;
-	}
-	$Sitemap .= '</sitemapindex>'.PHP_EOL;
-	return $Sitemap;
-}
-
-if ( count($Pages) > $SitemapPagination ) {
+if ( count($Pages) > $Sitewide['Sitemap']['Pagination'] ) {
 	$SitemapCount = 1;
-	while ( $SitemapCount <= ceil( count($Pages) / $SitemapPagination ) ) {
-		$SitemapPages = array_slice($Pages, ( $SitemapCount - 1 ) * $SitemapPagination, $SitemapCount * $SitemapPagination);
-		$Sitemap = Puff_Sitemap_Make($SitemapPages);
+	while ( $SitemapCount <= ceil( count($Pages) / $Sitewide['Sitemap']['Pagination'] ) ) {
+		$SitemapPages = array_slice($Pages, ( $SitemapCount - 1 ) * $Sitewide['Sitemap']['Pagination'], $SitemapCount * $Sitewide['Sitemap']['Pagination']);
+		$Sitemap = Sitemap_Make($SitemapPages);
 		$Result = file_put_contents($Sitewide['Root'].'sitemap_'.$SitemapCount.'.xml', $Sitemap);
 		if ( $Result ) {
 			echo 'Success: Generation and Write of Sitemap "'.$Sitewide['Root'].'sitemap_'.$SitemapCount.'.xml'.'" successful.'."\n";
@@ -67,7 +27,7 @@ if ( count($Pages) > $SitemapPagination ) {
 		}
 		$SitemapCount++;
 	}
-	$Sitemap = Puff_Sitemap_Index($SitemapCount);
+	$Sitemap = Sitemap_Index($SitemapCount);
 	$Result = file_put_contents($Sitewide['Root'].'sitemap.xml', $Sitemap);
 	if ( $Result ) {
 		echo 'Success: Generation and Write of Sitemap "'.$Sitewide['Root'].'sitemap.xml'.'" successful.'."\n";
@@ -76,7 +36,7 @@ if ( count($Pages) > $SitemapPagination ) {
 	}
 
 } else {
-	$Sitemap = Puff_Sitemap_Make($Pages);
+	$Sitemap = Sitemap_Make($Pages);
 	$Result = file_put_contents($Sitewide['Root'].'sitemap.xml', $Sitemap);
 	if ( $Result ) {
 		echo 'Success: Generation and Write of Sitemap "'.$Sitewide['Root'].'sitemap.xml'.'" successful.'."\n";
