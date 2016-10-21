78c503ec57b2fe3c0b86d4217a9d81cf06f35cef
Lewis Goddard <lewisgoddard@eustasy.org>
2016-10-21 22:43:44 +0100

    Update sitemap_make.php

--- _functions/core/sitemap_make.php
+++ _functions/core/sitemap_make.php
@@ -3,29 +3,39 @@
 function Sitemap_Make($SitemapPages) {
 	$Sitemap = '<?xml version="1.0" encoding="utf-8"?>'.PHP_EOL;
 	$Sitemap .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'.PHP_EOL;
+	
 	foreach ( $SitemapPages as $Item ) {
-		// TODO Responsive Priority and ChangeFreq
 		$Sitemap .= '	<url>'.PHP_EOL;
+		
+		// Link
 		$Sitemap .= '		<loc>'.$Item['Link'].'</loc>'.PHP_EOL;
+		
+		// Publish Date
 		if (
 			!empty($Item['Published']) &&
 			$Item['Published'] != '1970-01-01'
 		) {
 			$Sitemap .= '		<lastmod>'.$Item['Published'].'</lastmod>'.PHP_EOL;
 		}
+		
+		// Priority
 		$Priority = 1 - ( ( substr_count($Item['Link'], '/') - 3 ) / 10 );
 		if ( $Priority < 0.1 ) {
 			$Priortiy = 0.1;
 		}
 		$Sitemap .= '		<priority>'.$Priority.'</priority>'.PHP_EOL;
+		
+		// Change Frequency
 		if ( strpos($Item['Type'], 'Index') !== false ) {
 			$Changed = 'daily';
 		} else {
 			$Changed = 'weekly';
 		}
 		$Sitemap .= '		<changefreq>'.$Changed.'</changefreq>'.PHP_EOL;
+		
 		$Sitemap .= '	</url>'.PHP_EOL;
 	}
+	
 	$Sitemap .= '</urlset>'.PHP_EOL;
 	return $Sitemap;
 }
