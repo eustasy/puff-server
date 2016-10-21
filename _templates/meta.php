ba35d9586183f69cc2f2994ea64f655cad69fea7
Lewis Goddard <lewisgoddard@eustasy.org>
2016-10-21 22:34:50 +0100

    Create meta.php

---
+++ _templates/meta.php
@@ -0,0 +1,37 @@
+<!-- Meta Info -->
+<title><?php echo $Page['Title']; ?></title>
+<meta name="description"                content="<?php echo $Page['Description']; ?>">
+<meta name="author"                     content="<?php echo $Page['Author']; ?>">
+<meta name="theme-color"                content="<?php echo $Page['Theme Color']; ?>">
+<!-- Optimizations for Google -->
+<meta itemprop="name"                   content="<?php echo $Page['Title']; ?>" />
+<meta itemprop="description"            content="<?php echo $Page['Description']; ?>" />
+<meta itemprop="image"                  content="<?php echo $Page['Image']; ?>" />
+<!-- Optimizations for Facebook -->
+<meta property="og:title"               content="<?php echo $Page['Title']; ?>" />
+<meta property="og:description"         content="<?php echo $Page['Description']; ?>" />
+<meta property="og:image"               content="<?php echo $Page['Image']; ?>" />
+<!-- Optimizations for Twitter -->
+<meta name="twitter:title"              content="<?php echo $Page['Title']; ?>">
+<meta name="twitter:description"        content="<?php echo $Page['Description']; ?>">
+<meta name="twitter:image"              content="<?php echo $Page['Image']; ?>" />
+<meta name="twitter:site"               content="<?php echo $Page['Twitter Site']; ?>">
+<meta name="twitter:creator"            content="<?php echo $Page['Twitter Author']; ?>">
+<?php
+	if ( !empty($Page['Image']) ) {
+		echo '<meta name="twitter:card"               content="summary_large_image">';
+	} else {
+		echo '<meta name="twitter:card"               content="summary">';
+	}
+	echo "\n";
+?>
+<!-- iPinning -->
+<meta name="apple-mobile-web-app-title" content="<?php echo $Sitewide['Settings']['Site Title']; ?>">
+<!-- Android Pinning -->
+<link rel="manifest"                    href="/assets/manifest.json">
+<!-- Favicon -->
+<link rel="shortcut icon"               href="/assets/icons/favicon.ico">
+<link rel="apple-touch-icon"            href="/assets/icons/apple-touch-icon.png">
+<link rel="icon" type="image/png"       href="/assets/icons/favicon.png" sizes="256x256">
+<!-- Authorship -->
+<link rel="author"                      href="<?php echo $Page['Google+ Author']; ?>" title="<?php echo $Page['Author Name']; ?>"/>
