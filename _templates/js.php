68d8088269d5395c561f3ea25d7c5c0a616fae3d
Lewis Goddard <lewisgoddard@eustasy.org>
2016-10-21 22:36:00 +0100

    Create js.php

---
+++ _templates/js.php
@@ -0,0 +1,35 @@
+<!-- JavaScripts -->
+<?php puff_hook('head-js-pre'); ?>
+<script><?php require_once $Sitewide['Assets']['Internal']['JS'].'jQl.min.js'; ?></script>
+<script>jQl.loadjQ('<?php echo $Page['JQ']; ?>');</script>
+<?php
+	if (!empty($Page['JS'])) {
+		foreach ( $Page['JS'] as $Key => $Value ) {
+			if (
+				is_array($Value) &&
+				!empty($Value['internal'])
+			) {
+				echo '<script>'.PHP_EOL;
+				include_once $Key;
+				echo '</script>'.PHP_EOL;
+			} else if (
+				is_array($Value) &&
+				!empty($Value['library'])
+			) {
+				echo '<script>jQl.loadjQdep(\''.$Key.'\');</script>'.PHP_EOL;
+			} else if (
+				is_array($Value) &&
+				!empty($Value['async'])
+			) {
+				echo '<script src="'.$Key.'" async></script>'.PHP_EOL;
+			} else if ( is_array($Value) || empty($Value) ) {
+				echo '<script src="'.$Key.'"></script>'.PHP_EOL;
+			} else if ( !is_array($Value) ) {
+				echo '<script src="'.$Value.'"></script>'.PHP_EOL;
+			}
+		}
+	}
+	echo !empty($Page['Header']) ? $Page['Header'] : false;
+	puff_hook('head-js-post');
+	echo '<script>jQl.boot();</script>'.PHP_EOL;
+?>
