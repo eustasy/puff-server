9f6f9847024a1ba51b18ae9ae699cd3a66dabe75
Lewis Goddard <lewisgoddard@eustasy.org>
2017-10-18 17:44:29 +0100

    Delete javascripts.php

--- _hooks/head-js-pre/javascripts.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$Sitewide['Page']['JS'][$Sitewide['Assets']['Internal']['JS'].'external-links.js'];
-$Sitewide['Page']['JS'][$Sitewide['Assets']['Internal']['JS'].'smooth-scrolling.js'];
-$Sitewide['Page']['JS'][$Sitewide['Assets']['Internal']['JS'].'xss-warning.js'];
