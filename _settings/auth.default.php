2e28dec4f2fd7699ef19df97e81568fe128670d3
Lewis Goddard <lewisgoddard@eustasy.org>
2022-08-30 01:18:24 +0100

    Update auth.default.php

--- _settings/auth.default.php
+++ _settings/auth.default.php
@@ -7,13 +7,14 @@ $Sitewide['Settings']['Members']['Password Retention']['BCRYPT'] = true;
 // Hardware cost for BCRYPT
 $Sitewide['Settings']['Members']['Password Retention']['BCRYPT Cost'] = 10;
 // Support PLAIN Password Storage
-$Sitewide['Settings']['Members']['Password Retention']['PLAIN'] = true;
+$Sitewide['Settings']['Members']['Password Retention']['PLAIN'] = false;
 // Oldest date to keep a password from.
 // 365 days = 60*60*24*365 = 44676000
 // DO NOT SET TO 0
+// TODO Why not?
 $Sitewide['Settings']['Members']['Password Retention']['Oldest'] = 44676000;
 // Prompt the user when using an old password to try and log in.
-$Sitewide['Settings']['Members']['Password Retention']['Prompt on old password'] = false;
+$Sitewide['Settings']['Members']['Password Retention']['Prompt on old password'] = true;
 
 ////	Enable the LDAP Integration
 $Sitewide['Settings']['Members']['LDAP Integration']['Enabled'] = false;
